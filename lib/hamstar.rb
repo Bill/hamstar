398082badef199e1a8e0ee248fbbc8903b002f2a
Bill <bill.burcham@gmail.com>
2015-10-31 17:42:55 -0700

    reuse Hamster update_in

--- lib/hamstar.rb
+++ lib/hamstar.rb
@@ -13,17 +13,10 @@ module Hamstar
     if key_path.empty?
       raise ArgumentError, "must have at least one key in path"
     end
-    key = key_path[0]
-    if key == KLEENE_STAR
-      kleene_star(c, *key_path, &block)
+    if key_path[0] == KLEENE_STAR
+      kleene_star c, *key_path, &block
     else
-      if key_path.size == 1
-        new_value = block.call(c.fetch(key,nil))
-      else
-        value = c.fetch(key, Hamster::EmptyHash)
-        new_value = update_having(value, *key_path[1..-1], &block)
-      end
-      c.put(key, new_value)
+      c.update_in *key_path, &block
     end
   end
 
@@ -31,7 +24,7 @@ module Hamstar
     kp_nostar = Hamster.from(key_path)[1..-1] # drop kleene star
     c.keys.each do |key|
       kp = kp_nostar.unshift key # put key where kleene star was
-      c = update_having(c, *kp, &block)
+      c = update_having c, *kp, &block
     end
     c
   end
