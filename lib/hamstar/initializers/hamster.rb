947c602551d6c759a2584fd4db318d77f8666140
Bill <bill.burcham@gmail.com>
2015-11-03 07:19:15 -0800

    pared down Hamster monkey patch

--- lib/hamstar/initializers/hamster.rb
+++ lib/hamstar/initializers/hamster.rb
@@ -2,13 +2,6 @@ require 'hamster'
 
 module Hamster
   class Vector
-    # Vectors act a lot like associations. Why not give 'em a keys method?
-    def keys
-      (0..(size-1))
-    end
-    def values
-      self
-    end
     def each_pair
       each_with_index do |v,i|
         yield i,v
